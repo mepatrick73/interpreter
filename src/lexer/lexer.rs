f7b54bd97ba74dda366381606a5757b9a29bc489
mepatrick73 <pameu17@ulaval.ca>
2024-08-24 18:02:53 -0400

    adding utf8 support aswell as some unit tests

--- src/lexer/lexer.rs
+++ src/lexer/lexer.rs
@@ -4,7 +4,6 @@ struct Lexer {
     readposition: usize,
     ch: Option<char>,
 }
-
 impl Lexer {
     pub fn new(input: String) -> Self {
         let mut l = Lexer {
@@ -13,18 +12,69 @@ impl Lexer {
             readposition: 0,
             ch: None,
         };
-        l.readChar();
+        l.read_char();
         l
     }
 
-    pub fn readChar(&mut self) {
-        if self.readposition > self.input.len() {
+    pub fn read_char(&mut self) {
+        if self.readposition >= self.input.len() {
             self.ch = None;
         } else {
             let ch = self.input[self.readposition..].chars().next();
             self.position = Some(self.readposition);
-            self.readposition += ch.map(|_| 1).unwrap_or(0);
+
+            if let Some(character) = ch {
+                self.readposition += character.len_utf8();
+            }
+
             self.ch = ch;
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_lexer_initialization() {
+        let input = String::from("hello");
+        let lexer = Lexer::new(input.clone());
+
+        assert_eq!(lexer.input, input);
+        assert_eq!(lexer.position, Some(0));
+        assert_eq!(lexer.readposition, 1);
+        assert_eq!(lexer.ch, Some('h'));
+    }
+
+    #[test]
+    fn test_read_char() {
+        let input = String::from("hell😃o");
+        let mut lexer = Lexer::new(input.clone());
+
+        lexer.read_char();
+        assert_eq!(lexer.position, Some(1));
+        assert_eq!(lexer.ch, Some('e'));
+
+        lexer.read_char();
+        assert_eq!(lexer.position, Some(2));
+        assert_eq!(lexer.ch, Some('l'));
+
+        lexer.read_char();
+        assert_eq!(lexer.position, Some(3));
+        assert_eq!(lexer.ch, Some('l'));
+
+        lexer.read_char();
+        assert_eq!(lexer.position, Some(4));
+        assert_eq!(lexer.ch, Some('😃'));
+
+        lexer.read_char();
+        assert_eq!(lexer.position, Some(8));
+        assert_eq!(lexer.ch, Some('o'));
+
+        lexer.read_char();
+        assert_eq!(lexer.position, Some(8));
+        assert_eq!(lexer.readposition, 9);
+        assert_eq!(lexer.ch, None);
     }
 }
